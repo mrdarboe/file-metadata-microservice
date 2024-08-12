cc33c792f37b1b8bf0ec8e15193b1c9c94d38e14
mrdarboe <gsdarboe1@gmail.com>
2024-08-12 12:35:47 +0000

    implementation using multer

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'})
 require('dotenv').config()
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,8 +13,20 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), async function(req, res){
+  const uploadedFile = req.file;
+  try {
+    return res.json({"name": uploadedFile.originalname, "type": uploadedFile.mimetype, "size": uploadedFile.size})
+  } catch(err) {
+    return res.send(err)
+  }
+})
+/**
+ * multer handle file upload??
+ * 
+ * POST /api/fileanalyse
+ * res.json({file.name, file.type, file.size(bytes)})
+ */
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
